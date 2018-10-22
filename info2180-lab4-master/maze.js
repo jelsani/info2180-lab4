f1d359547c774fdba29cde4241e988c4324cd2cf
Jelani Bryan-Hall <jelanikush@gmail.com>
2018-10-22 09:47:27 -0500

    Update info2180-lab4-master/maze.js
    Changing and seeing what works from what doesn't and what I understand

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -1,29 +1,17 @@
-var div = document.querySelector("#div");
-var delta = 10; // the thickness of the hovered border area
+var border = document.getElementById("maze");
 
-div.onmousemove = function(e) {
-    var rect = div.maze();
-    var x = e.clientX - rect.left,      // the relative mouse postion to the element
-        y = e.clientY - rect.top,       // ...
-        w = rect.right - rect.left,     // width of the element
-        h = rect.bottom - rect.top;     // height of the element
-        
-   var c = "";                          // which cursor to use
-   if(y < delta) c += "n";              // north
-   else if( y > h - delta) c += "s";    // south
-   if(x < delta) c += "w";              // west
-   else if(x > w - delta) c += "e";     // east
-   
-   if(c)                                // if we are hovering at the border area (c is not empty)
-       div.style.cursor = c + "-resize"; // set the according cursor
-   else                                 // otherwise
-       div.style.cursor = "pointer";    // set to pointer
+var bor = 4;
+
+border.onclick = function(e) {
+  if (e.target !== e.currentTarget) return;
+  console.log("border-clicked")
 }
 
-#div {
-  background-color: red;
-  width: 100px;
-  height: 100px;
+border.onmouseover = function(e) {
+  y = e.offsetY;
+  
+  if (y <= bor || y >= this.offsetHeight - bor) c = "row"
+  else c = "col"
+
+  this.style.cursor = c + "-resize";
 }
-<div id="div"></div>
-<br>Psst! Hover at the border area! Corners too.
